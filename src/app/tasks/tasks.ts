80d0b93d9e3c21d77dd41d827e879f8fbe5682c9
shalini <s.rathore43@gmail.com>
2026-03-16 16:42:11 +0530

    ldgjlfjg

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -13,11 +13,31 @@ export class Tasks {
 today: Date;
   tasks: TaskModel[];
   taskService: TaskService;
+  queued!: number;
+   queuedTasks!: number;
+  queuedHeaderMapping: any = {
+    '=0': 'No tasks',
+    '=1': 'One task',
+    'other': '# tasks'
+  };
+  timerMinutes: number=25;
   constructor() {
     this.taskService = new TaskService();
     this.tasks = this.taskService.taskStore;
     this.today = new Date();
   }
-  ngOnInit(): void {  }
-
+   ngOnInit(): void {
+    this.updateQueuedTasks();
+  }
+ toggleTask(task: TaskModel): void {
+    task.queued = !task.queued;
+    this.updateQueuedTasks();
+  }
+    private updateQueuedTasks() {
+    this.queuedTasks = this.tasks
+      .filter((task: TaskModel) => task.queued)
+      .reduce((no: number, queuedTask: TaskModel) => {
+        return no + queuedTask.noRequired;
+      }, 0)
+  }
 }
